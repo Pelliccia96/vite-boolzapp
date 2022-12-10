7a9ecf2db9f80784cf91e4159963f8c17b4fcd25
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-12-10 19:43:33 +0100

    Adds axios resp in Main.vue

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -42,14 +42,10 @@
                             </div>
                             <div>
                                 <div class="fw-semibold">{{ users.name.first }}</div>
-                                <div v-if="users.messages > 0" class="small text-secondary">
-                                    {{ users.messages[users.messages - 1].message }}</div>
-                                <div v-else>{{ "" }}</div>
+                                <div class="small text-secondary">{{ users.email }}</div>
                             </div>
                         </div>
-                        <div v-if="users.messages > 0" class="small text-secondary pe-2">
-                            {{ users.messages[users.messages - 1].date }}</div>
-                        <div v-else>{{ "" }}</div>
+                        <div class="small text-secondary pe-2">{{ users.gender }}</div>
                     </div>
                 </div>
             </div>
@@ -77,9 +73,9 @@
             </div>
             <!-- Text Section -->
             <div class="dx-row d-flex flex-column flex-grow-1 pt-5 scrollbar-y">
-                <div v-if="selectedUser !== null" v-for="message, i in selectedUser.messages"
-                    :class="message.status === 'sent' ? 'my-msg' : 'user-msg'">{{ message.message }} <small
-                        class="msg-date ps-2">{{ message.date }}</small>
+                <div v-if="selectedUser !== null" v-for="message, i in store.messagesList"
+                    :class="message.name === 'sent' ? 'my-msg' : 'user-msg'">{{ message.name }} <small
+                        class="msg-date ps-2">{{ message.email }}</small>
                     <div class="ms-3">
                         <div class="btn-group">
                             <button class="btn btn-secondary btn-sm dropdown-toggle" type="button"
@@ -125,179 +121,6 @@ export default {
             selectedUser: null,
             search: "",
             dateTime: luxon.DateTime,
-            usersList: [
-                {
-                    name: "Michele",
-                    avatar: "img/avatar_1.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: "10/01/2020 15:30:55",
-                            message: "Hai portato a spasso il cane?",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 15:50:00",
-                            message: "Ricordati di dargli da mangiare",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 16:15:22",
-                            message: "Tutto fatto!",
-                            status: "received",
-                        },
-                    ],
-                },
-                {
-                    name: "Fabio",
-                    avatar: "img/avatar_2.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: "20/03/2020 16:30:00",
-                            message: "Ciao come stai?",
-                            status: "sent",
-                        },
-                        {
-                            date: "20/03/2020 16:30:55",
-                            message: "Bene grazie! Stasera ci vediamo?",
-                            status: "received",
-                        },
-                        {
-                            date: "20/03/2020 16:35:00",
-                            message: "Scusa, devo fare la spesa.",
-                            status: "received",
-                        },
-                    ],
-                },
-                {
-                    name: "Samuele",
-                    avatar: "img/avatar_3.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: "28/03/2020 10:10:40",
-                            message: "La Marianna va in campagna",
-                            status: "received",
-                        },
-                        {
-                            date: "28/03/2020 10:20:10",
-                            message: "Sicuro di non aver sbagliato chat?",
-                            status: "sent",
-                        },
-                        {
-                            date: "28/03/2020 16:15:22",
-                            message: "Ah scusa!",
-                            status: "received",
-                        },
-                    ],
-                },
-                {
-                    name: "Luisa",
-                    avatar: "img/avatar_4.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: "10/01/2020 15:30:55",
-                            message: "Lo sai che ha aperto una nuova pizzeria?",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 15:50:00",
-                            message: "Si, ma preferirei andare al cinema",
-                            status: "received",
-                        },
-                    ],
-                },
-                {
-                    name: "Alessandro L.",
-                    avatar: "img/avatar_5.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: "10/01/2020 15:30:55",
-                            message: "Hai portato a spasso il cane?",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 15:50:00",
-                            message: "Ricordati di dargli da mangiare",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 16:15:22",
-                            message: "Tutto fatto!",
-                            status: "received",
-                        },
-                    ],
-                },
-                {
-                    name: "Claudia",
-                    avatar: "img/avatar_6.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: "10/01/2020 15:30:55",
-                            message: "Hai portato a spasso il gatto?",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 15:50:00",
-                            message: "Ricordati di dargli da mangiare",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 16:15:22",
-                            message: "Ci penso io!",
-                            status: "received",
-                        },
-                    ],
-                },
-                {
-                    name: "Federico",
-                    avatar: "img/avatar_7.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: "10/01/2020 15:30:55",
-                            message: "Hai portato a spasso il topo?",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 15:50:00",
-                            message: "Ricordati di dargli da mangiare",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 16:15:22",
-                            message: "Non dire altro!",
-                            status: "received",
-                        },
-                    ],
-                },
-                {
-                    name: "Davide",
-                    avatar: "img/avatar_8.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: "10/01/2020 15:30:55",
-                            message: "Hai portato a spasso il lupo?",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 15:50:00",
-                            message: "Ricordati di dargli da mangiare",
-                            status: "sent",
-                        },
-                        {
-                            date: "10/01/2020 16:15:22",
-                            message: "Allora, calma!",
-                            status: "received",
-                        },
-                    ],
-                },
-            ],
         }
     },
     methods: {
